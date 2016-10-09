7cfb8548c6eeca75db87ea51930b37a800b899a0
Klas Karlsson <klaskarlsson@hotmail.com>
2016-10-09 11:03:02 +0200

    update

--- collections/Geosupportsystem/processing/SelectObjectsWithinDistance.py
+++ collections/Geosupportsystem/processing/SelectObjectsWithinDistance.py
@@ -1,6 +1,6 @@
 ##Select objects within distance=name
-##vljfrnlager=vector
-##somliggerinom=number0
-##frnlager=vector
+##Select from layer=vector
+##Within Distance=number0
+##That are near Layer=vector
 outputs_QGISFIXEDDISTANCEBUFFER_1=processing.runalg('qgis:fixeddistancebuffer', frnlager,somliggerinom,5.0,False,None)
 outputs_QGISSELECTBYLOCATION_1=processing.runalg('qgis:selectbylocation', vljfrnlager,outputs_QGISFIXEDDISTANCEBUFFER_1['OUTPUT'],['within'],0.0,0,None)
