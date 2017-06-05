433fdc49257b500227e1dfc46f8b60060a773735
Klas Karlsson <klakar@users.noreply.github.com>
2017-06-05 10:34:53 +0200

    Some fixes
    Among other things fixes the default value for visibility.

--- collections/Geosupportsystem/processing/enabelAdvancedLabels.py
+++ collections/Geosupportsystem/processing/enabelAdvancedLabels.py
@@ -1,4 +1,4 @@
-##Add Advanced Labeling Fields=name
+##Add Advanced Labeling Fields To Selected Layer=name
 from PyQt4.QtCore import QVariant
 from qgis.utils import iface
 from qgis.core import QgsField, QgsPalLayerSettings
@@ -21,24 +21,24 @@ if not lager.isEditable():
     iface.actionToggleEditing().trigger()
 lager.dataProvider().addAttributes([QgsField("Text_X", QVariant.Double, 'double', 10, 5),
 QgsField("Text_Y", QVariant.Double, 'double', 10, 5), QgsField("Text_Ri", QVariant.Double, 'double', 3, 2), QgsField("Text_Vis", QVariant.Int, "", 3)])
-lager.setDefaultValueExpression(lager.fieldNameIndex('Text_Vis'), '1')
 lager.updateFields()
+lager.setDefaultValueExpression(lager.fieldNameIndex("Text_Vis"), '1')
 palager = QgsPalLayerSettings()
 palager.readFromLayer(lager)
 palager.enabled = True
 palager.setDataDefinedProperty(QgsPalLayerSettings.PositionX, True, False, '', "Text_X")
 palager.setDataDefinedProperty(QgsPalLayerSettings.PositionY, True, False, '', "Text_Y")
 palager.setDataDefinedProperty(QgsPalLayerSettings.Rotation, True, False, '', "Text_Ri")
-palager.setDataDefinedProperty(QgsPalLayerSettings.Show, True, True, '', "Text_Vis")
+palager.setDataDefinedProperty(QgsPalLayerSettings.Show, True, False, '', "Text_Vis")
 setColumnVisibility( lager, 'Text_X', False)
 setColumnVisibility( lager, 'Text_Y', False)
 setColumnVisibility( lager, 'Text_Ri', False)
 setColumnVisibility( lager, 'Text_Vis', False)
 palager.writeToLayer(lager)
-iface.mapCanvas().refresh()
 rader = lager.getFeatures()
 for rad in rader:
     rad['Text_Vis'] = 1
     lager.updateFeature(rad)
 lager.commitChanges()
 lager.startEditing()
+iface.mapCanvas().refresh()
