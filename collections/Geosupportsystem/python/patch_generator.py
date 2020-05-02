604584edb0b011d102326aa8573b9f5abeca167c
Klas Karlsson <klakar@users.noreply.github.com>
2020-05-02 19:58:18 +0200

    Update patch_generator.py

--- collections/Geosupportsystem/python/patch_generator.py
+++ collections/Geosupportsystem/python/patch_generator.py
@@ -1,3 +1,42 @@
+'''
+Made by Klas Karlsson
+Uses a polygon layer with suitable patch sized rectangles at representative areas.
+The table must have three columns.
+1. id
+2. Label text (any name but [string])
+3. Rotation (if you want to tweak the patch [float] should be default 0.0)
+
+Before you run the script from the python consol in QGIS:
+- Turn the layer visibility off (unless you want to show it in the patches)
+- Have the patch layer selected (the script is run on the active layer)
+- Save the project somewhere (patches are generated at this location)
+
+The script generates lines with HTML in the Python consol. Copy this code!
+
+In the layout, add a HTML frame and paste the code as source.
+
+Style the apperance with css in the "User Stylesheet" settings:
+
+******** Example CSS **********
+.patch {
+   width: 110px;
+}
+.title {
+   font-family: Arial; 
+   font-size: 12pt;
+   text-align: left;
+   padding: 0px 16px;
+}
+table {
+   border-spacing: 0px 4px;
+}
+body {
+   margin: 0;
+}
+*******************************
+
+You can tweak the HTML code if you want multiple columns. It's not that hard.
+'''
 import re
 
 layer = iface.activeLayer()
