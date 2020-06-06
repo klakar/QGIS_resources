43da20c256fb21d258baf653f1a9f470f2b24527
Klas Karlsson <klakar@users.noreply.github.com>
2020-06-06 09:41:54 +0200

    Delete SelectObjectsWithinDistance.py

--- collections/Geosupportsystem/processing/SelectObjectsWithinDistance.py
+++
@@ -1,6 +0,0 @@
-##Select objects within distance=name
-##Select from layer=vector
-##Within Distance=number0
-##That are near Layer=vector
-outputs_QGISFIXEDDISTANCEBUFFER_1=processing.runalg('qgis:fixeddistancebuffer', frnlager,somliggerinom,5.0,False,None)
-outputs_QGISSELECTBYLOCATION_1=processing.runalg('qgis:selectbylocation', vljfrnlager,outputs_QGISFIXEDDISTANCEBUFFER_1['OUTPUT'],['within'],0.0,0,None)
