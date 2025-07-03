04830f820f839f2694142b10174197cf4342a197
Klas Karlsson <klakar@users.noreply.github.com>
2025-07-03 12:02:55 +0200

    Update Simplify_QGIS_GUI.py

--- collections/Geosupportsystem/python/Simplify_QGIS_GUI.py
+++ collections/Geosupportsystem/python/Simplify_QGIS_GUI.py
@@ -3,15 +3,23 @@
 # Start the Python console (ctrl+alt+P)
 # Show the editor, load and run this script
 #
+
+# List toolbars to make visible (all others will be hidden)
 showToolbars = "mFileToolBar mAttributesToolBar mDigitizeToolBar mHelpToolBar mMapNavToolBar mDataSourceManagerToolBar"
-prev = None
+
+# Go through all toolbars
 for x in iface.mainWindow().findChildren(QToolBar): 
     if x.objectName() in showToolbars:
         x.setVisible(True)
         x.setIconSize(QSize(32,32))
     else:
         x.setVisible(False)
+
+# List panels to make visible (all others will be hidden)
 showDockWidgets = "Layers LayerStyling Browser"
+
+# Go through all panels and place some of them at specific locations
+prev = None
 for d in iface.mainWindow().findChildren(QDockWidget): 
     if d.objectName() in showDockWidgets:
         if d.objectName() in "Browser LayerStyling":
